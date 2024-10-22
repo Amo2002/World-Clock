92571d023bdb09cf54f72849f1229b5cea0acdbf
Amo2002 <163900007+Amo2002@users.noreply.github.com>
2024-10-22 16:58:42 +0200

    tried other timezone cdn for the correct times

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-funtion updateTime() {
+function updateTime() {
+  //Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  let johannesburgTime = document.querySelector(".time");
+  let johannesburgDate = document.querySelector(".date");
+  let johannesburgDateAndTime = moment().tz("Africa/Johannesburg");
+  johannesburgTime.innerHTML = johannesburgDateAndTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  johannesburgDate.innerHTML = johannesburgDateAndTime.format("MMMM Do YYYY");
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisTime = document.querySelector(".time");
+  let parisDate = document.querySelector(".date");
+  let parisDateAndTime = moment().tz("Europe/Paris");
+  parisTime.innerHTML = parisDateAndTime.format("h:mm:ss [<small>]A[</small>]");
+  parisDate.innerHTML = parisDateAndTime.format("MMMM Do YYYY");
 
-//Paris
-let parisElement = document.querySelector("#paris");
-let parisTime = document.querySelector(".time");
-let parisDate = document.querySelector(".date");
-let parisDateAndTime = moment().tz("Europe/Paris");
-parisTime.innerHTML = parisDateAndTime.format("h:mm:ss [<small>]A[</small>]");
-parisDate.innerHTML = parisDateAndTime.format("MMMM Do YYYY");
-//Johannesburg
-let johannesburgElement = document.querySelector("#johannesburg");
-let johannesburgTime = document.querySelector(".time");
-let johannesburgDate = document.querySelector(".date");
-let johannesburgDateAndTime = moment().tz("Africa/Johannesburg");
-johannesburgTime.innerHTML = johannesburgDateAndTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
-johannesburgDate.innerHTML = johannesburgDateAndTime.format("MMMM Do YYYY");
-
-//Singapore
-let singaporeElement = document.querySelector("#singapore");
-let singaporeTime = document.querySelector(".time");
-let singaporeDate = document.querySelector(".date");
-let singaporeDateAndTime = moment().tz("Asia/Singapore");
-singaporeTime.innerHTML = singaporeDateAndTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
-singaporeDate.innerHTML = singaporeDateAndTime.format("MMMM Do YYYY");
-
+  //Singapore
+  let singaporeElement = document.querySelector("#singapore");
+  let singaporeTime = document.querySelector(".time");
+  let singaporeDate = document.querySelector(".date");
+  let singaporeDateAndTime = moment().tz("Asia/Singapore");
+  singaporeTime.innerHTML = singaporeDateAndTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  singaporeDate.innerHTML = singaporeDateAndTime.format("MMMM Do YYYY");
 }
+
 updateTime();
 setInterval(updateTime, 1000);
